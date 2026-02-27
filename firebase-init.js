b9ccb2089b4df95d94e6c2ba85b971265e57896b
ilsico <florent.bousquetpro@gmail.com>
2026-02-27 17:58:47 +0100

    feat: add permissions for Bash commands and implement thumbnail scheduling design

--- firebase-init.js
+++ firebase-init.js
@@ -7,7 +7,6 @@ try {
     storageBucket: 'moodboard-app-b21b9.firebasestorage.app',
     messagingSenderId: '467105545598',
     appId: '1:467105545598:web:72fceeaa8c4d0c949b472f',
-    databaseURL: 'https://moodboard-app-b21b9-default-rtdb.firebaseio.com',
   });
   window._fbDb = firebase.database();
 } catch (e) {

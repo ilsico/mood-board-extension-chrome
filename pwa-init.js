39f9e427d95d360e4445e8c97b75e433a7dfec4c
ilsico <florent.bousquetpro@gmail.com>
2026-05-21 03:41:18 +0200

    fix: pwa-init uniquement hors contexte extension

--- pwa-init.js
+++ pwa-init.js
@@ -1,6 +1,8 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js');
+if (location.protocol !== 'chrome-extension:') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js');
+  }
+  window.addEventListener('load', function () {
+    document.documentElement.requestFullscreen && document.documentElement.requestFullscreen().catch(function () {});
+  });
 }
-window.addEventListener('load', function () {
-  document.documentElement.requestFullscreen && document.documentElement.requestFullscreen().catch(function () {});
-});
